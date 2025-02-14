f69af55fdc9376fa39a490632fe331dffbe8f814
IvanovMike2023 <ivanov.michail.2023@yandex.ru>
2025-02-14 15:35:16 +0300

    pl

--- js/game.js
+++ js/game.js
@@ -114,7 +114,6 @@ export class Game {
                 newPosition.x++
                 break
         }
-
         const isInsideGrid =newPosition.x > 0 && newPosition.x < this.#settings.gridSize.rowsCount &&
             newPosition.y > 0 && newPosition.y < this.#settings.gridSize.columnsCount
         const isCellFreeOtherPlayer = newPosition.x!=this.#playerPositions[playerNumber] && newPosition.y!=this.#playerPositions[!playerNumber]
@@ -126,6 +125,5 @@ export class Game {
         }
         this.#playerPositions[playerNumber] = newPosition
     }
-
 }
 
